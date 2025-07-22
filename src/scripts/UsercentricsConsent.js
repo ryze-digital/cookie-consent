4e42c52c5d282198251020700ca270dbb92d3161
David Schaer <d.schaer@ryze-digital.de>
2025-07-22 14:53:21 +0200

    feat: Migrate to RuleSet loader & add div support
    KLKB-288

--- src/scripts/UsercentricsConsent.js
+++ src/scripts/UsercentricsConsent.js
@@ -1,71 +1,76 @@
-import { CookieConsent } from './CookieConsent.js';
+import { CookieConsent } from './CookieConsent.js'
 
 export class UsercentricsConsent extends CookieConsent {
-    #categoryMap = {
+    categoryMap = {
         preferences: 'functional',
         marketing: 'marketing',
-        statistics: 'marketing'
-    };
+        statistics: 'statistics',
+        sale_data: 'sale_of_personal_data',
+        targeted_advertising: 'targeted_advertising'
+    }
 
     constructor() {
         super({
             bannerIdentifier: '#uc-main-dialog',
             privacyUrlIdentifier: '[data-cookie-preference-center]',
             consentReadDelay: 120
-        });
-        this._initEvents();
-        this._checkConsent();
+        })
+        this._initEvents()
+        this._checkConsent()
     }
 
     _initEvents() {
-        window.addEventListener('UC_UI_INITIALIZED', () => {
-            return this._checkConsent();
-        });
-        window.addEventListener('UC_CONSENT', () => {
-            return this._checkConsent();
-        });
-        window.addEventListener('consent_status', () => {
-            return this._checkConsent();
-        });
-        window.addEventListener('UC_UI_CMP_EVENT', (e) => {
-            if (['ACCEPT_ALL', 'SAVE'].includes(e.detail.type)) {
-                this._checkConsent();
-            }
-        });
-        document.addEventListener('click', (e) => {
+        window.addEventListener('UC_UI_INITIALIZED', () => this._checkConsent())
+        window.addEventListener('UC_CONSENT', () => this._checkConsent())
+        window.addEventListener('consent_status', () => this._checkConsent())
+        window.addEventListener('UC_UI_CMP_EVENT', e => {
+            if (['ACCEPT_ALL', 'SAVE'].includes(e.detail.type)) this._checkConsent()
+        })
+        document.addEventListener('click', e => {
             if (e.target.matches(this.options.privacyUrlIdentifier)) {
-                this._openPrivacyCenter();
+                this._openPrivacyCenter()
             }
-        });
+        })
     }
 
     _checkConsent() {
         setTimeout(() => {
-            const entries = window.dataLayer?.filter((item) => {
-                return item.event === 'consent_status';
-            }) || [];
-            const entry = entries[entries.length - 1] || {};
-            const categories = entry?.ucCategory || {};
+            const entries = window.dataLayer?.filter(item => item.event === 'consent_status') || []
+            const entry = entries[entries.length - 1] || {}
+            const categories = entry?.ucCategory || {}
 
-            Object.keys(this.#categoryMap).forEach((key) => {
-                this.options.consent[key] = categories[this.#categoryMap[key]] === true;
-            });
-            const consentModel = this.options.consent;
-
-            document.querySelectorAll('iframe[data-src][data-cookieconsent]').forEach((iframe) => {
-                const consents = iframe.getAttribute('data-cookieconsent');
-
+            console.log('Usercentrics Kategorien (ausgelesen):', categories)
+            Object.keys(this.categoryMap).forEach(key => {
+                this.options.consent[key] = categories[this.categoryMap[key]] === true
+            })
+            const consentModel = this.options.consent
+            document.querySelectorAll('iframe[data-src][data-cookieconsent]').forEach(iframe => {
+                const consents = iframe.getAttribute('data-cookieconsent')
                 if (this.constructor.isConsentRequired(consents, consentModel) && iframe.getAttribute('src')) {
-                    iframe.removeAttribute('src');
+                    iframe.removeAttribute('src')
                 }
-            });
-            this._emitConsentStatusEvent();
-        }, this.options.consentReadDelay);
+            })
+            this._emitConsentStatusEvent()
+            this.toggleContentElements(consentModel)
+        }, this.options.consentReadDelay)
     }
 
     _openPrivacyCenter() {
         if (window.UC_UI?.showSecondLayer) {
-            window.UC_UI.showSecondLayer();
+            window.UC_UI.showSecondLayer()
         }
+    }
+
+    toggleContentElements(consentModel) {
+        document
+            .querySelectorAll('[data-cookieconsent]:not(script):not([data-cookieconsent="ignore"])')
+            .forEach(el => {
+                const consents = el.getAttribute('data-cookieconsent')
+                if (this.constructor.isConsentRequired(consents, consentModel)) {
+                    el.style.display = 'none'
+                } else {
+                    el.style.display = ''
+                }
+            })
     }
 }
