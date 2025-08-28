5db125a0eb22caea55322e78381b10caf114dc8e
Tim Schiemann <t.schiemann@ryze-digital.de>
2025-08-28 13:43:07 +0200

    fix: Prevent js errors with incorrect markup

--- src/scripts/CookieConsentTagManager.js
+++ src/scripts/CookieConsentTagManager.js
@@ -42,7 +42,7 @@ class CookieConsentTagManager {
         this.#nonce = this.#element.dataset.nonce;
         this.#acceptedCookies = {};
 
-        if (!this.#trackingId) {
+        if (this.#element === null || !this.#trackingId) {
             return;
         }
 
